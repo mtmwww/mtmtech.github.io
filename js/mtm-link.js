981a8fb02391d104acf8df546b0b9beac7904805
Kimi Shen <kimi.shen@mtmtech.com.tw>
2017-03-23 15:12:39 +0800

    Update kit page with description.

---
+++ js/mtm-link.js
@@ -0,0 +1,12 @@
+$('.mtconnect04s').each(function () {
+    var link = $(this).html();
+    $(this).contents().wrap('<a href="//blog.mtmtech.com.tw/mtconnect04s"></a>');
+});
+$('.mtconnect02').each(function () {
+    var link = $(this).html();
+    $(this).contents().wrap('<a href="//blog.mtmtech.com.tw/mtconnect02"></a>');
+});
+$('.mtsense').each(function () {
+    var link = $(this).html();
+    $(this).contents().wrap('<a href="//blog.mtmtech.com.tw/sensor"></a>');
+});
