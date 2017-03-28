f11e1db3a2eaa9a8af87c07f068fff68c448057a
Kimi Shen <kimi.shen@mtmtech.com.tw>
2017-03-28 12:25:14 +0800

    Update kit price and description.

--- js/mtm-link.js
+++ js/mtm-link.js
@@ -1,12 +1,20 @@
-$('.mtconnect04s').each(function () {
+$('.link-mtconnect04s').each(function () {
     var link = $(this).html();
     $(this).contents().wrap('<a href="//blog.mtmtech.com.tw/mtconnect04s"></a>');
 });
-$('.mtconnect02').each(function () {
+$('.link-mtconnect02').each(function () {
     var link = $(this).html();
     $(this).contents().wrap('<a href="//blog.mtmtech.com.tw/mtconnect02"></a>');
 });
-$('.mtsense').each(function () {
+$('.link-mtsense').each(function () {
     var link = $(this).html();
     $(this).contents().wrap('<a href="//blog.mtmtech.com.tw/sensor"></a>');
+});
+$('.link-nrf51sdk').each(function () {
+    var link = $(this).html();
+    $(this).contents().wrap('<a href="//www.nordicsemi.com/eng/Products/Bluetooth-low-energy/nRF5-SDK"></a>');
+});
+$('.link-nrf51sdk-keil-tutor').each(function () {
+    var link = $(this).html();
+    $(this).contents().wrap('<a href="//blog.mtmtech.com.tw/2015/12/08/nrf51822-devenv-en/"></a>');
 });
