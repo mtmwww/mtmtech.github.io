51c2c81d694088993344a45a03ecef6fb0d70290
Kimi <kimi@mtmtech.com.tw>
2017-06-03 13:02:43 +0800

    Kit: Add AKM datasheet link

--- js/mtm-link.js
+++ js/mtm-link.js
@@ -18,3 +18,15 @@ $('.link-nrf51sdk-keil-tutor').each(function () {
     var link = $(this).html();
     $(this).contents().wrap('<a href="//blog.mtmtech.com.tw/2015/12/08/nrf51822-devenv-en/"></a>');
 });
+$('.link-spec-ak09912').each(function () {
+    var link = $(this).html();
+    $(this).wrap('<a href="//www.akm.com/akm/en/file/datasheet/AK09912C.pdf"></a>');
+});
+$('.link-spec-ak9750').each(function () {
+    var link = $(this).html();
+    $(this).wrap('<a href="//www.akm.com/akm/en/product/datasheet1/?partno=AK9750"></a>');
+});
+$('.link-spec-ak09970').each(function () {
+    var link = $(this).html();
+    $(this).wrap('<a href="//www.akm.com/akm/en/product/detail/0059/"></a>');
+});
