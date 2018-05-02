34021dde369fc76adcd8a9c1a0e2fe4aeba4721f
Ron Chan <34265628+ckrchan@users.noreply.github.com>
2018-05-02 01:24:36 -0400

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,12 @@
-$(document).ready(function(){
-  $('.dropdown-submenu a.test').on("click", function(e){
-    $(this).next('ul').toggle();
-    e.stopPropagation();
-    e.preventDefault();
+ $(document).ready(function() {
+    var $links = $('.dropdown-submenu a.test').on("click", function(e) {
+      var submenu = $(this).next();
+        $subs.not(submenu).hide()
+        submenu.toggle();
+      //$(this).next('ul').toggle();
+      e.stopPropagation();
+      e.preventDefault();
+    });
+    var $subs = $links.next();
+
   });
-});
